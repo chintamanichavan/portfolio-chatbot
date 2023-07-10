ba514bc9945da4a90dae876cf99fa2394cbd88f9
Chintamani Chavan <55659337+chintamanichavan@users.noreply.github.com>
2023-07-09 22:32:18 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+// App.js
+
+import React from 'react';
+import ChatInterface from './ChatInterface';
+
+const App = () => {
+  return (
+    <div>
+      <h1>My Portfolio</h1>
+      {/* Add other components and sections of your portfolio */}
+      <ChatInterface />
+    </div>
+  );
+};
+
+export default App;
